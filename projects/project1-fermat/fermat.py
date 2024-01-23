9bf8c918ab29e2fea70fa5eb70cd9601c2d5738f
Cannon Farr <cfarr@nerdunited.com>
2024-01-23 16:24:57 -0700

    feat: project 1

--- projects/project1-fermat/fermat.py
+++ projects/project1-fermat/fermat.py
@@ -6,15 +6,21 @@ def prime_test(N, k):
 
 # You will need to implement this function and change the return value.
 def mod_exp(x, y, N): 
-	return 1
+	if y == 0:
+		return 1
+	z = mod_exp(x, y//2, N)
+	if y % 2 == 0:
+		return (z*z) % N
+	else:
+		return (x*z*z) % N
 	
 # You will need to implement this function and change the return value.   
 def fprobability(k):
-    return 0.0
+    return 1 - (.5 ** k)
 
 # You will need to implement this function and change the return value.   
 def mprobability(k):
-    return 0.0
+    return 1 - (1/(4**k))
 
 # You will need to implement this function and change the return value, which should be
 # either 'prime' or 'composite'.
@@ -23,6 +29,10 @@ def mprobability(k):
 # random.randint(low,hi) which gives a random integer between low and
 # hi, inclusive.
 def fermat(N,k):
+	for i in range(k):
+		a = random.randint(2,N-1)
+		if mod_exp(a, N-1, N) != 1:
+			return 'composite'
 	return 'prime'
 
 # You will need to implement this function and change the return value, which should be
@@ -32,4 +42,31 @@ def fermat(N,k):
 # random.randint(low,hi) which gives a random integer between low and
 #  hi, inclusive.
 def miller_rabin(N,k):
-	return 'composite'
+
+	sequence = []
+	exp = N-1
+	for i in range(k):
+		a = random.randint(2,N-1)
+		if mod_exp(a, N-1, N) != 1:
+			return 'composite'
+		print('a: ' + str(a))
+		
+		while exp % 2 == 0:
+			modResult = mod_exp(a, exp, N)
+			print('exp: ' + str(exp))
+			print('mod: ' + str(modResult))
+			if modResult == 1:
+				sequence.append(1)
+			elif modResult == N-1 and sequence[-1] == 1:
+				return 'prime'
+			else:
+				return 'composite'
+			
+			exp /= 2
+		return 'prime'
+		
+
+		
+
+		
+		
