0dfb2958516126ef96f43d6192405f25b7380eef
Cannon Farr <cfarr@nerdunited.com>
2024-03-19 19:11:46 -0600

    feat: project 4 code

--- projects/project4-gene-sequencing/GeneSequencing.py
+++ projects/project4-gene-sequencing/GeneSequencing.py
@@ -38,22 +38,30 @@ class GeneSequencing:
 			seq1, seq2 = seq2, seq1
 		n = self.MaxCharactersToAlign if len(seq1) > self.MaxCharactersToAlign else len(seq1)
 		m = self.MaxCharactersToAlign if len(seq2) > self.MaxCharactersToAlign else len(seq2)
+		
 
 		if self.banded:
-			score, backtrack = self.bandedAlgorithm(seq1[:n], seq2[:m], MAXINDELS)
+			if m - n > MAXINDELS * 2 + 1:
+				return {'align_cost':float('inf'), 'seqi_first100':'No Alignment Possible.', 'seqj_first100':'No Alignment Possible.'}
+			score, backtrack = self.bandedAlgorithm(seq1[:n], seq2[:m], n, m, MAXINDELS)
+			alignment1, alignment2 = self.constructAlignmentBanded(seq1[:n], seq2[:m], n, m, MAXINDELS, backtrack)
 		else:
-			score, backtrack = self.unrestrictedAlgorithm(seq1[:n], seq2[:m])
+			score, backtrack = self.unrestrictedAlgorithm(seq1[:n], seq2[:m], n, m)
+			alignment1, alignment2 = self.constructAlignmentUnrestricted(seq1[:n], seq2[:m], n, m, backtrack)
 
-		alignment1, alignment2 = self.constructAlignment(seq1[:n], seq2[:m], backtrack)
 
 		return {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
 	
-	def unrestrictedAlgorithm(self, seq1: str, seq2: str):
+	def unrestrictedAlgorithm(self, seq1: str, seq2: str, n: int, m: int):
 
 		# Initialize variables
-		n = len(seq1)
-		m = len(seq2)
 		backtrack = []
+		for i in range(n + 1):
+			backtrack.append([('')] * (m + 1))
+			backtrack[i][0] = ('DEL',i,0)
+		for i in range(m + 1):
+			backtrack[0][i] = ('INS',0,i)
+		backtrack[0][0] = ('SUB',0,0)
 
 		# Create a 2D array to store the cost of the alignment
 		arr = self.initArray(n, m, -1)
@@ -70,11 +78,11 @@ class GeneSequencing:
 				
 				# Determine the operation and add it to the backtrack list
 				if val == 0:
-					backtrack.append('INS')
+					backtrack[i][j] = ('INS',i,j)
 				elif val == 1:
-					backtrack.append('SUB')
+					backtrack[i][j] = ('SUB',i,j)
 				else:
-					backtrack.append('DEL')
+					backtrack[i][j] = ('DEL',i,j)
 		
 		# Return the score and the backtrack list
 		return arr[n][m], backtrack
@@ -82,12 +90,17 @@ class GeneSequencing:
 
 
 
-	def bandedAlgorithm(self, seq1: str, seq2: str, band: int):
+	def bandedAlgorithm(self, seq1: str, seq2: str, n: int, m: int, band: int):
 		#Initialize variables
-		n = len(seq1)
-		m = len(seq2)
 		k = 2 * band + 1
 		backtrack = []
+		for i in range(n + 1):
+			backtrack.append([('')] * (k)) # k + 1
+		for i in range(band + 1): # k + 1
+			backtrack[i][0] = ('DEL',i,0)
+			backtrack[0][i] = ('INS',0,i)
+		backtrack[0][0] = ('SUB',0,0)
+		
 
 		# Create a 2D array to store the cost of the alignment
 		arr = self.initArray(n, m, band, k)
@@ -101,10 +114,10 @@ class GeneSequencing:
 		for i in range(1, n + 1):
 
 			# Col is the index of the column in the n * k array wheras j is the index of the column in the original sequence
-			col = 1
+			col = 0
 
 			# Determine how many columns to skip based upon the current row
-			if i > n - band:
+			if i > m - band:
 				skip += 1
 
 			# Loop through the columns
@@ -114,28 +127,28 @@ class GeneSequencing:
 				index = col + skip
 
 				# If we are at the beginning, then we skip the column indexes that are less than 1
-				if j < 1:
+				if j < 0:
 					continue
 
 				# If we are at the end, then we skip the column indexes that are greater than k
-				if index > k:
+				if index >= k:
 					break
 
 				# If we are in the rows that don't have the full band, we check the values to the left, up, and upLeft
-				if i <= band + 1 or i > n - band:
+				if i <= band or i > n - band:
 					arr[i][index], val = self.min_with_index(self.left(arr, i, index) + INDEL, (self.upLeft(arr,i,index) + self.cost(seq1[i - 1], seq2[j - 1])), self.up(arr, i, index) + INDEL)
 
 				# If we are in the rows that have the full band, we check the values to the left, up, and upRight
 				else:
-					arr[i][index], val = self.min_with_index(self.left(arr, i, index) + INDEL, (self.up(arr, i, index) + self.cost(seq1[i - 1], seq2[j - 1])), (float('inf') if j >= k else self.upRight(arr, i, j) + INDEL))
+					arr[i][index], val = self.min_with_index(self.left(arr, i, index) + INDEL, (self.up(arr, i, index) + self.cost(seq1[i - 1], seq2[j - 1])), (float('inf') if j >= k - 1 else self.upRight(arr, i, j) + INDEL))
 				
 				# Determine the operation and add it to the backtrack list
 				if val == 0:
-					backtrack.append('INS')
+					backtrack[i][index] = ('INS',i,j)
 				elif val == 1:
-					backtrack.append('SUB')
+					backtrack[i][index] = ('SUB',i,j)
 				else:
-					backtrack.append('DEL')
+					backtrack[i][index] = ('DEL',i,j)
 
 				# Increment the column index
 				col += 1
@@ -156,9 +169,10 @@ class GeneSequencing:
 
 			#initialize the first row and column for banded
 			for i in range(n + 1):
-				arr.append([float('inf')] * (k + 1))
+				arr.append([float('inf')] * (k))
+			arr[0][0] = 0
 			for i in range(band + 1):
-				arr[i][0] = i * INDEL
+			# 	arr[i][0] = i * INDEL
 				arr[0][i] = i * INDEL
 
 		else:
@@ -172,23 +186,63 @@ class GeneSequencing:
 
 		return arr
 	
-	def constructAlignment(self, seq1: str, seq2: str, backtrack: list):
-	# 	last = backtrack.pop()
-	# 	alignment1 = ''
-	# 	alignment2 = ''
-	# 	while len(backtrack) > 0:
-	# 		i, j = backtrack.pop()
-	# 		if i == last[0] - 1 and j == last[1] - 1:
-	# 			alignment1 = seq1[i] + alignment1
-	# 			alignment2 = seq2[j] + alignment2
-	# 		elif i == last[0] and j == last[1] - 1:
-	# 			alignment1 = '-' + alignment1
-	# 			alignment2 = seq2[j] + alignment2
-	# 		else:
-	# 			alignment1 = seq1[i] + alignment1
-	# 			alignment2 = '-' + alignment2
-	# 	return alignment1, alignment2
-		return '', ''
+
+	def constructAlignmentBanded(self, seq1: str, seq2: str, n: int, m: int, band: int, backtrack: list[tuple[str, int, int]]):
+		i = n
+		k = (2 * band + 1) - 1
+		cur = backtrack[i][k]
+		alignment1 = ''
+		alignment2 = ''
+		while i > 0 or k > 0:
+			if cur[0] == 'INS':
+				alignment1 = '-' + alignment1
+				alignment2 = seq2[cur[2] - 1] + alignment2
+				k -= 1
+			elif cur[0] == 'DEL':
+				alignment1 = seq1[cur[1] - 1] + alignment1
+				alignment2 = '-' + alignment2
+				if i <= band or i > n - band:
+					i -= 1
+				else:
+					i -= 1
+					k += 1
+			else:
+				alignment1 = seq1[cur[1] - 1] + alignment1
+				alignment2 = seq2[cur[2] - 1] + alignment2
+				if i <= band or i > n - band:
+					i -= 1
+					k -= 1
+				else:
+					i -= 1
+			cur = backtrack[i][k]
+		return alignment1, alignment2
+
+
+	def constructAlignmentUnrestricted(self, seq1: str, seq2: str, n: int, m: int, backtrack: list[tuple[str, int, int]]):
+		i = n
+		j = m
+		cur = backtrack[i][j][0]
+		alignment1 = ''
+		alignment2 = ''
+		while i > 0 or j > 0:
+			if cur == 'INS':
+				alignment1 = '-' + alignment1
+				alignment2 = seq2[j - 1] + alignment2
+				j -= 1
+			elif cur == 'DEL':
+				alignment1 = seq1[i - 1] + alignment1
+				alignment2 = '-' + alignment2
+				i -= 1
+			else:
+				alignment1 = seq1[i - 1] + alignment1
+				alignment2 = seq2[j - 1] + alignment2
+				i -= 1
+				j -= 1
+			cur = backtrack[i][j][0]
+		return alignment1, alignment2
+	
+
+
 		
 	def left(self, arr: list, i: int, j: int):
 		return arr[i][j - 1]
